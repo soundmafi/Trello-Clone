7511b3d1efb6db66c20db4634136420469b90498
soundmafi <43148286+soundmafi@users.noreply.github.com>
2022-02-17 21:45:56 +0300

    JS/SCSS/HTML: added clock

--- js/script.js
+++ js/script.js
@@ -254,3 +254,15 @@ function rebuild(){
     counterTasks(taskBase);
     renderList(taskBase);
 }
+
+
+window.onload = function() {
+    setInterval(function() {
+      let seconds = new Date().getSeconds();
+      document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
+      let minutes = new Date().getMinutes();
+      document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
+      let hours = new Date().getHours();
+      document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
+    }, 1000);
+  }
